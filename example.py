c9e299bc5da10294dc70d20c89ae03132665ce08
Saniya Anklesaria <saniyaanklesaria@Saniyas-MacBook-Pro.local>
2025-01-14 12:00:54 -0500

    Working get corporate-activity by activity id

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+import re
 import streamlit as st
 from langchain_community.agent_toolkits.openapi import planner
 from langchain_openai import ChatOpenAI
@@ -18,7 +19,7 @@ def create_openapi_agent():
 
     llm = ChatOpenAI(model_name="gpt-4o", temperature=0.3)
 
-    with open("specs/events.yaml", "r") as file:
+    with open("specs/corporate_activity.yaml", "r") as file:
         raw_events_api_spec = yaml.safe_load(file)
 
     events_api_spec = reduce_openapi_spec(raw_events_api_spec)
