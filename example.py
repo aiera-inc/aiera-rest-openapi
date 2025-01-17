7747de67c12406acde90ff4309b01dc88348a31f
Saniya Anklesaria <saniyaanklesaria@saniyas-mbp.lan>
2025-01-17 11:33:39 -0500

    Get filings content by bloomberg ticker

--- example.py
+++ example.py
@@ -18,7 +18,7 @@ def create_openapi_agent():
 
     llm = ChatOpenAI(model_name="gpt-4o", temperature=0.3)
 
-    with open("specs/events.yaml", "r") as file:
+    with open("specs/content.yaml", "r") as file:
         raw_events_api_spec = yaml.safe_load(file)
 
     events_api_spec = reduce_openapi_spec(raw_events_api_spec)
@@ -28,6 +28,7 @@ def create_openapi_agent():
         requests_wrapper,
         llm,
         allow_dangerous_requests=True,
+        handle_parsing_errors=True,
     )
     return agent
 
